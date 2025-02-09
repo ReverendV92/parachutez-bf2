39abb3965ea3a6f7f8f3fb880e6fad192b1bc946
ReverendV92 <ReverendV92@users.noreply.github.com>
2025-02-09 10:06:14 -0600

    Removed blacklisted .sw.vtx files

---
+++ .content/cleaner.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+echo Checking the directory....
+:CLEAN
+echo Cleaning all crap files...
+del /F /S *.cache
+del /F /S *.ztmp
+del /F /S *.sw.vtx
+del /F /S *.xbox.vtx
+del /F /S *.log
+del /F /S *.DS_Store
+del /F /S *__MACOSX
+del /F /S *.mdmp
+del /F /S *.prt
+del /F /S *.lin
+del /F /S *.json
+del /F /S desktop.ini
+echo ------------------------------------------------------
+echo Removed all crap files!
+goto EXIT
+
+:EXIT
+exit
